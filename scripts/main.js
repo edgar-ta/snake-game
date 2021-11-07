46b4cb075fa39c6d0b5fd82f89b82685f04dc140
Edgar Trejo Avila <edgar.avila.rro@gmail.com>
2021-11-07 16:35:40 -0600

    Added Board Look
    I added the html skeleton and css code for
    the look of the board an start button.
    These ones are centered respect to the screen

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,17 @@
-console.log("Hello world!");
+/**
+ * removes all divs (called blocks) inside the board
+ * and adds another n^2
+ * @param {number} n measure for the number of blocks to add
+ */
+function addBlocks(n = 10) {
+    let board = document.querySelector(".board");
+    document.querySelectorAll(".board__block").forEach(block => block.remove());
+    for (let i = 0; i< n ** 2; i++) {
+        let block = document.createElement("div");
+        block.setAttribute("class", "board__block");
+        board.appendChild(block);
+    }
+}
+
+
+addBlocks();
